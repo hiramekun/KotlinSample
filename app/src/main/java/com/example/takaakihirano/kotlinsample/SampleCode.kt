6d11071a27bd156cf6566a878433519f9ed619f0
hiramekun <thescript1210@gmail.com>
2017-03-21 15:35:49 +0900

    First commit

---
+++ app/src/main/java/com/example/takaakihirano/kotlinsample/SampleCode.kt
@@ -0,0 +1,12 @@
+package com.example.takaakihirano.kotlinsample
+
+/**
+ * Created by takaakihirano on 2017/03/21.
+ */
+class SampleCode {
+
+    fun calculate(int: Int): Int {
+        val cal = int + 1
+        return cal
+    }
+}
