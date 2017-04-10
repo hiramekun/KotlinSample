aa57aa70a10c6e4695461bf38cfd1cc3a2a4696f
hiramekun <thescript1210@gmail.com>
2017-04-11 00:06:12 +0900

    Show listview in MainActivity

--- app/src/main/java/com/example/takaakihirano/kotlinsample/MainActivity.kt
+++ app/src/main/java/com/example/takaakihirano/kotlinsample/MainActivity.kt
@@ -2,22 +2,27 @@ package com.example.takaakihirano.kotlinsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
 import com.example.takaakihirano.kotlinsample.model.Article
 import com.example.takaakihirano.kotlinsample.model.User
-import com.example.takaakihirano.kotlinsample.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val articleView = ArticleView(applicationContext)
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "太郎"),
+                dummyArticle("Java入門", "二郎"))
 
-        articleView.setArticle(Article(id = "123",
-                title = "Kotlin入門",
-                url = "http://www.example.com/articles/123",
-                user = User(id = "456", name = "太郎", profileImageUrl = "")))
-
-        setContentView(articleView)
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlinlang.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
