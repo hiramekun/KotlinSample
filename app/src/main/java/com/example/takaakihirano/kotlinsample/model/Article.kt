d109ad1986545c16c117b85b81606964ea0eae96
hiramekun <thescript1210@gmail.com>
2017-04-11 23:06:04 +0900

    Implement methods to be percelable

--- app/src/main/java/com/example/takaakihirano/kotlinsample/model/Article.kt
+++ app/src/main/java/com/example/takaakihirano/kotlinsample/model/Article.kt
@@ -1,5 +1,8 @@
 package com.example.takaakihirano.kotlinsample.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /**
  * Created by takaakihirano on 2017/04/08.
  */
@@ -7,4 +10,27 @@ package com.example.takaakihirano.kotlinsample.model
 data class Article(val id: String,
                    val title: String,
                    val url: String,
-                   val user: User)
+                   val user: User) : Parcelable {
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Article> = object : Parcelable.Creator<Article> {
+            override fun createFromParcel(source: Parcel): Article = source.run {
+                Article(readString(), readString(), readString(), readParcelable(Article::class.java.classLoader))
+            }
+
+            override fun newArray(size: Int): Array<Article?> = arrayOfNulls(size)
+        }
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.run {
+            writeString(id)
+            writeString(title)
+            writeString(url)
+            writeParcelable(user, flags)
+        }
+    }
+
+    override fun describeContents(): Int = 0
+}
